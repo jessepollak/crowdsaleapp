5af8389ffe135b5ded40738bc9d14c976db43637
Joey Krug <joeykrug@gmail.com>
2015-08-04 00:05:57 -0500

    trying something for clef

--- app.js
+++ app.js
@@ -83,6 +83,12 @@ app.get('/', function(req, res) {
   }
 });
 
+app.get('/login', function(req, res) {
+  res.render('login') {
+    csrf_token: createToken(generateSalt(10), process.env.CSRFSALT)
+  }
+})
+
 // handle ethereum address form submission
 app.post('/', function(req, res) {
 
